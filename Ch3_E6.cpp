18489f0f1be0e5dfa3bfb625f0056be3a68e5751
makeshift6600 <j.l.cannady04@gmail.com>
2025-09-21 14:30:34 -0400

    The actual program

--- Ch3_E6.cpp
+++ Ch3_E6.cpp
@@ -1,20 +1,25 @@
-// Ch3_E6.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+File name:Ch3_E6
+Date:09/2025
+Programmer name:Jamen Cannady
+Requirements: ask how many cookies the user wants to make, then display the number of cups each ingrediant needs
+*/
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int cookies;
+    double OSugar = 1.5, OButter = 1, OFlour = 2.75;
+    double Sugar = OSugar / 48, Butter = OButter / 48, Flour = OFlour / 48;
+
+    cout << "How many cookies would you like to make?" << endl;
+    cin >> cookies;
+    double NSugar = Sugar * cookies, NButter = Butter * cookies, NFlour = Flour * cookies;
+
+    cout << "To make " << cookies << " cookies you need " << NSugar << " cups of sugar, " << NButter << " cups of butter, and " << NFlour << " cups of flour.";
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
